556dca2d40e13b2ef3178987b0c4e753d69c52f3
Michele Maione <mikymaione@hotmail.it>
2019-10-24 16:18:12 +0200

    modifiche guida sferica

--- Project/BeStrongBeTheLast/Assets/Scripts/Car/AutoSfera.cs
+++ Project/BeStrongBeTheLast/Assets/Scripts/Car/AutoSfera.cs
@@ -3,14 +3,16 @@
 public class AutoSfera : MonoBehaviour
 {
 
+    public Transform LookHere, Position;
+
     public Rigidbody sphere;
     public Vector3 diffSphere;
     public GameObject KartModel;
 
     public float steering, accellaration;
-    private float currentSpeed, currentRotate;
 
-    public Transform LookHere, Position;
+
+    private float currentSpeed, currentRotate, maxS;
 
     private GeneralCar generalCar;
 
@@ -22,26 +24,30 @@ public class AutoSfera : MonoBehaviour
         MyCamera.positionTarget = Position;
 
         generalCar = GetComponent<GeneralCar>();
+        maxS = generalCar.Speed / 15;
     }
 
     private void Update()
     {
         KartModel.transform.position = sphere.transform.position - diffSphere;
+        //KartModel.transform.eulerAngles = new Vector3(KartModel.transform.eulerAngles.x, sphere.transform.eulerAngles.y, KartModel.transform.eulerAngles.z);
 
         var H = Input.GetAxis("Horizontal");
 
-        currentRotate = Mathf.Lerp(currentRotate, steering * H, Time.deltaTime * 4f);
-        currentSpeed = Mathf.SmoothStep(currentSpeed, accellaration, Time.deltaTime * 12f);
+        currentRotate = Mathf.Lerp(currentRotate, steering * H, Time.deltaTime);
+        currentSpeed = Mathf.SmoothStep(currentSpeed, accellaration, Time.deltaTime);
 
-        if (currentSpeed > generalCar.Speed / 20)
-            currentSpeed = generalCar.Speed / 20;
+        if (currentSpeed > maxS)
+            currentSpeed = maxS;
+
+        // Debug.Log(currentRotate + " | " + currentSpeed);
     }
 
     private void FixedUpdate()
     {
+        //sphere.transform.RotateAroundLocal(Vector3.up, currentRotate * Time.deltaTime * 5f);
+        KartModel.transform.eulerAngles = Vector3.Lerp(KartModel.transform.eulerAngles, new Vector3(0, KartModel.transform.eulerAngles.y + currentRotate, 0), Time.deltaTime);
         sphere.AddForce(-1 * KartModel.transform.forward * currentSpeed, ForceMode.Acceleration);
-
-        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
     }
 
 
