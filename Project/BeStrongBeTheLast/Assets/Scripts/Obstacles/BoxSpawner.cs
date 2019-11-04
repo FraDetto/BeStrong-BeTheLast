6cf0450d642c3bbf7317120bfefa575c84a49585
Michele Maione <mikymaione@hotmail.it>
2019-11-04 15:10:25 +0100

    aggiunta licenza

--- Project/BeStrongBeTheLast/Assets/Scripts/Obstacles/BoxSpawner.cs
+++ Project/BeStrongBeTheLast/Assets/Scripts/Obstacles/BoxSpawner.cs
@@ -1,73 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+MIT License
+Copyright (c) 2019: Francesco Dettori, Jacopo Frasson, Riccardo Lombardi, Michele Maione
+Author: Jacopo Frasson
+Contributors:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System.Collections;
 using UnityEngine;
 
 public class BoxSpawner : MonoBehaviour
 {
+
     public GameObject BoxAccelerating, BoxSlowing;
     public bool GreenTrueRedFalse;
+
     int startSpawn = 0;
     int startColour;
 
-    // Start is called before the first frame update
     void Start()
     {
-        startColour = Random.Range(0,10);
-        if(startColour < 5)
-        {
-            GreenTrueRedFalse = true;
-        }
-        else
-        {
-            GreenTrueRedFalse = false;
-        }
-       
+        startColour = Random.Range(0, 10);
+        GreenTrueRedFalse = (startColour < 5);
         StartCoroutine(spawnBox());
     }
 
-   IEnumerator spawnBox()
+    IEnumerator spawnBox()
     {
         GameObject go;
+
         if (startSpawn == 0)
         {
             int waitTime = Random.Range(1, 3);
             yield return new WaitForSeconds(waitTime);
         }
-        
+
         while (true)
         {
-            if (GreenTrueRedFalse)
-            {
-                go = Instantiate(BoxAccelerating, transform.position, Quaternion.identity);
-                go.transform.Rotate(0, -90, 0);
-                //GreenTrueRedFalse = false;
-                startColour = Random.Range(0, 10);
-                if (startColour < 5)
-                {
-                    GreenTrueRedFalse = true;
-                }
-                else
-                {
-                    GreenTrueRedFalse = false;
-                }
-            }
-            else
-            {
-                go = Instantiate(BoxSlowing, transform.position, Quaternion.identity);
-                go.transform.Rotate(0, -90, 0);
-                //GreenTrueRedFalse = true;
-                startColour = Random.Range(0, 10);
-                if (startColour < 5)
-                {
-                    GreenTrueRedFalse = true;
-                }
-                else
-                {
-                    GreenTrueRedFalse = false;
-                }
-            }
+            go = Instantiate(GreenTrueRedFalse ? BoxAccelerating : BoxSlowing, transform.position, Quaternion.identity);
+            go.transform.Rotate(0, -90, 0);
+
+            startColour = Random.Range(0, 10);
+            GreenTrueRedFalse = (startColour < 5);
+
             yield return new WaitForSeconds(1.6f);
         }
-       
     }
+
 }
