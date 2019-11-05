ff23c126ebbeb186c0f8b249afb4a23fa4831f75
Jacopo Frasson <jacopo.frasson.95@gmail.com>
2019-11-05 17:29:10 +0100

    Moving Ramp

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Obstacles/PlaneObstacles/MovingRamp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRamp : MonoBehaviour
+{
+    public Transform ramp;
+    public float speed = 10;
+    private bool down = true;
+    private int frames = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        if (ramp.transform.rotation.x <= 0.123f && ramp.transform.rotation.x >= 0)
+        {
+            if (frames != 0)
+            {
+                frames++;
+                if (frames == 180)
+                    frames = 0;
+            }
+            else
+            {
+                if (down == true)
+                    transform.RotateAround(ramp.position, ramp.right, speed * Time.deltaTime);
+                else
+                    transform.RotateAround(ramp.position, ramp.right, -speed * Time.deltaTime);
+            }
+
+            
+            
+        }
+        else
+        {
+            if (ramp.transform.rotation.x > 0.123f)
+            {
+                down = false;
+                transform.RotateAround(ramp.position, ramp.right, -speed * Time.deltaTime);
+                frames++;
+            } else if (ramp.transform.rotation.x < 0)
+            {
+                down = true;
+                transform.RotateAround(ramp.position, ramp.right, speed * Time.deltaTime);
+                frames++;
+            }
+        }
+    }
+}
