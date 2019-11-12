62d9021cd8f6e7cb8267df01e064f60b269639a7
Riccardo Lombardi <lomb.richard@gmail.com>
2019-11-12 18:39:57 +0100

    Primi passi con le abilità

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Abilities/AbilityController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityController : MonoBehaviour
+{
+    [SerializeField] private Slider powerGauge;
+    [SerializeField] private Text selectedProjectileText;
+    [SerializeField] private float regenSpeed;
+    [SerializeField] private GameObject trishot;
+    [SerializeField] private GameObject homing;
+    [SerializeField] private GameObject bouncing;
+    [SerializeField] private GameObject attracting;
+    [SerializeField] private Transform frontSpawnpoint;
+    [SerializeField] private Transform rearSpawnpoint;
+
+    private GameObject selectedProjectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerGauge.value = 0;
+        selectedProjectile = trishot;
+        selectedProjectileText.text = "Trishot";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerGauge.value += regenSpeed * Time.deltaTime;
+        if(Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.W) && powerGauge.value >= 0.5f)
+        {
+            Instantiate(selectedProjectile, frontSpawnpoint.position, frontSpawnpoint.rotation);
+        }
+        if(Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.S) && powerGauge.value >= 0.5f)
+        {
+            Instantiate(selectedProjectile, rearSpawnpoint.position, rearSpawnpoint.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            selectedProjectile = homing;
+            selectedProjectileText.text = "Homing";
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedProjectile = trishot;
+            selectedProjectileText.text = "Trishot";
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            selectedProjectile = bouncing;
+            selectedProjectileText.text = "Bouncing";
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedProjectile = attracting;
+            selectedProjectileText.text = "Attracting";
+        }
+    }
+}
---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Abilities/TrishotBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrishotBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
