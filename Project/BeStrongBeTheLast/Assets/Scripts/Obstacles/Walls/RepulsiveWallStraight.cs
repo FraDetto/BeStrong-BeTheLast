b3ac72ed90f80e3be80c22e082900362f565c925
Jacopo Frasson <jacopo.frasson@mail.polimi.it>
2019-11-13 17:53:19 +0100

    Wall Stub 1

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Obstacles/Walls/RepulsiveWallStraight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepulsiveWallStraight : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("CPU"))
+        { 
+            var kartController = other.transform.parent.GetComponentInChildren<aKartController>();
+            kartController.Accelerate(2f);
+        }
+    }
+}
