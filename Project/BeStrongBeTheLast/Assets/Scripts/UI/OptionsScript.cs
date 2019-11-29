9a9b8010ff48a8387fa1a76e056143da93da074e
Jacopo Frasson <jacopo.frasson@mail.polimi.it>
2019-11-29 18:33:29 +0100

    Working options menu

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/UI/OptionsScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsScript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    private Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+    public Toggle fullscreenToggle;
+    private void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+        fullscreenToggle.isOn = Screen.fullScreen;
+    }
+
+    public void SetResolution(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MainVolume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
