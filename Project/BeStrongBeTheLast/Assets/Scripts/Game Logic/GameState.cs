91de0b9a5238e960efe3cefe3ddbc825af4a67f3
Jacopo Frasson <jacopo.frasson@mail.polimi.it>
2019-12-01 18:17:59 +0100

    Menu can now launch a race
    GameState singleton, track and kart selection, still problems with kart initialization

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Game Logic/GameState.cs
@@ -0,0 +1,61 @@
+﻿/*
+MIT License
+Copyright (c) 2019 Team Lama: Carrarini Andrea, Cerrato Loris, De Cosmo Andrea, Maione Michele
+Author: Frasson Jacopo
+Contributors: 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static GameState Instance;
+    
+    //CURRENT RACE OPTIONS
+    private string playerChampName, selectedTrackName;
+
+    void Awake ()   
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy (gameObject);
+        }
+    }
+
+    public static GameState getInstance()
+    {
+        return Instance;
+    }
+
+    public bool setPlayerChamp(string name)
+    {
+        playerChampName = name;
+        return true;
+        //Add check to see if prefab available, if not return false
+    }
+    
+    public string getPlayerChamp()
+    {
+        return playerChampName;
+    }
+    
+    public bool setCurrentTrack(string name)
+    {
+        selectedTrackName = name;
+        return true;
+        //Add check to see if prefab available, if not return false
+    }
+
+    public string getCurrentTrack()
+    {
+        return selectedTrackName;
+    }
+}
