686faeb89c1c640facb62fd45cf83dea6dc6f0f1
FraDetto <francesco030996@gmail.com>
2019-12-08 18:13:49 +0100

    Added tower script for electricity

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Obstacles/radioTowerTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radioTowerTrigger : MonoBehaviour
+{
+    
+    public float acceleration;
+    public int respawnElecTime;
+    public GameObject[] particlesToDis;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.root.GetComponentInChildren<KartController>().Accelerate(acceleration);
+        StartCoroutine(disableElectricity());
+    }
+
+    IEnumerator disableElectricity()
+    {
+        this.gameObject.GetComponent<Collider>().enabled = false;
+        foreach(GameObject part in particlesToDis)
+        {
+            part.SetActive(false);
+        }
+        yield return new WaitForSeconds(respawnElecTime);
+
+        foreach (GameObject part in particlesToDis)
+        {
+            part.SetActive(true);
+        }
+        this.gameObject.GetComponent<Collider>().enabled = false;
+    }
+}
