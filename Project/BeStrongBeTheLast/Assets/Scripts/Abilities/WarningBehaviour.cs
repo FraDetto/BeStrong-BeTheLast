58bce6ac90b59882cccd716b827d0b92caf0f7fd
Riccardo Lombardi <lomb.richard@gmail.com>
2019-12-09 01:16:06 +0100

    lil fix

--- Project/BeStrongBeTheLast/Assets/Scripts/Abilities/WarningBehaviour.cs
+++ Project/BeStrongBeTheLast/Assets/Scripts/Abilities/WarningBehaviour.cs
@@ -13,6 +13,8 @@ public class WarningBehaviour : MonoBehaviour
     {
         if(other.CompareTag("Projectile"))
         {
+            //Si lo so che fa cagare, si potrebbe sistemare con una bella classe astratta "Abilità" con il campo user, ma la pipeline di chiamate è così complessa ora che non mi sogno neanche di metterci mani.
+            //Be my guest ;-)
             if((other.transform.parent.name.StartsWith("Trishot") && !other.GetComponent<SingleShotBehaviour>().trishotBehaviour.user.Equals(transform.root.gameObject)) 
                 || (other.name.StartsWith("Homing") && !other.GetComponent<HomingBehaviour>().user.Equals(transform.root.gameObject))
                 || (other.name.StartsWith("Bouncing") && !other.GetComponent<BouncingBehaviour>().user.Equals(transform.root.gameObject)))
