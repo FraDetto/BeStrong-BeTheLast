4ca2ff5109470f372cd6917ebf6eaf4d18076061
Jacopo Frasson <jacopo.frasson.95@gmail.com>
2019-12-09 12:28:34 +0100

    Shortcut wall fix

---
+++ Project/BeStrongBeTheLast/Assets/Scripts/Obstacles/Walls/ShortcutInstantClose.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortcutInstantClose : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        var go = other.gameObject.transform.root.gameObject;
+        if (GB.CompareORTags(go, "Player", "CPU"))
+        {
+            transform.GetComponent<Collider>().isTrigger = false;
+        }
+    }
+}
