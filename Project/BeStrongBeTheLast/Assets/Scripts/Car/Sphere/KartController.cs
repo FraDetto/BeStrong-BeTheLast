6a68949144c52903ade14186824e07bbf0c46f21
Michele Maione <mikymaione@hotmail.it>
2019-12-16 16:45:13 +0100

    testing rotating: ok

--- Project/BeStrongBeTheLast/Assets/Scripts/Car/Sphere/KartController.cs
+++ Project/BeStrongBeTheLast/Assets/Scripts/Car/Sphere/KartController.cs
@@ -6,7 +6,6 @@ Contributors:
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -151,7 +150,9 @@ public sealed class KartController : aBSBTLKart
                     float angle = steerAngle(lookAtDest);
                     bool turn = Mathf.Abs(angle) > 10f;
                     bool left = angle < 0;
-                    driftAxis = (turn) ? ((left) ? 1 : -1) : 0;
+
+                    driftAxis = turn ? (left ? 1 : -1) : 0;
+
                     if (Mathf.Abs(angle) > 70)
                     {
                         //Possibly add some conditions regarding driftHeatingValue here, to avoid unreasonable use of the drift function
